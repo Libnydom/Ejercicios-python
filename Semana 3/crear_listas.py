32cb32e415e0563783d825569fa6ed1997b30656
Libny <114311509+Libnydom@users.noreply.github.com>
2026-01-26 19:21:07 -0600

    Ejercicio 1, semana 3
    Creación de listas

---
+++ Semana 3/crear_listas.py
@@ -0,0 +1,7 @@
+#| 1   | Crear listas, acceder elementos | Lista de 5 frutas, imprime la primera, la última, y cuántas hay        |
+
+lista_frutas = ["Fresa", "Uva", "Manzana", "Pera", "Sandía"]
+
+print(lista_frutas[0])
+print(lista_frutas[4])
+print(len(lista_frutas))
