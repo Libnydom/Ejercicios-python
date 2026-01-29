71fcee40a0f6ab5bfe58a7e76a3c207e7031d2af
Libny <114311509+Libnydom@users.noreply.github.com>
2026-01-28 19:32:34 -0600

    metodo_lista

---
+++ Semana 3/metodos_listas.py
@@ -0,0 +1,12 @@
+#| 3   | Métodos de listas | Dada una lista de números, encuentra el mayor, el menor y el promedio  |
+
+lista_numeros=[1,2,3,4,5,6]
+num_mayor=max(lista_numeros)
+num_menor=min(lista_numeros)
+num_prom= sum(lista_numeros) / len(lista_numeros)
+
+print(lista_numeros)
+print(num_mayor)
+print(num_menor)
+print(num_prom)
+
